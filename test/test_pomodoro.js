29c037a88d3d36ae97fc2339a3b1274206037ff1
Austin "Paco" Rainwater <hey@paco.wtf>
2023-12-30 19:07:35 +0000

    Add a test for TDD

---
+++ test/test_pomodoro.js
@@ -0,0 +1,6 @@
+import { assert } from 'chai';
+describe('ActivityInventory', () => {
+    it('should be able to add a task', () => {
+        assert.equal(1, 1);
+    });
+});
---
+++ test/test_pomodoro.ts
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+
+describe('ActivityInventory', () => {
+    it('should be able to add a task', () => {
+        assert.equal(1, 1);
+    });
+})
